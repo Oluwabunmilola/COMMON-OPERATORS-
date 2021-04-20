f945c095e2c2bca3d633ec2b9edf1e4c519a7342
Precious <oluwabunmipreciousajayi@gmail.com>
2021-04-20 10:30:01 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace COMMON_OPERATORS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Console.WriteLine("Hello World!");
+
+            // +,-,*,/,%
+            //Arithemetic operators.
+            // int numerator=10;
+            // int denominator=2;
+            // int result=numerator + denominator;
+            // Console.WriteLine(result);
+
+            //For comparism, doubble equal to == is used.
+            // for assignment, Single equal to = is used
+
+            //        string myName="Precious";
+            //        string myPass="1234";
+            //        string nameInput=Console.ReadLine();
+            //        string passInput=Console.ReadLine();
+
+            //    //    if(myName==nameInput && myPass==passInput)
+            //        if(!(myName==nameInput && myPass==passInput))
+            //        {
+            //            Console.WriteLine("True");
+
+
+            // int x = 20;
+            // int y = 10;
+            // if (x > y)
+            // {
+
+            //     Console.WriteLine("True");
+             
+        //  int a=10;
+        //  int b=a;
+        //   a++;
+        // Console.WriteLine(a); 
+
+        //   
+        
+        //   int a=10;
+        //  int b=a;
+        //   a+=5;
+        // Console.WriteLine(a); 
+
+        //  
+        
+        //     int a=10;
+        //  int b=a;
+        //   a*=5;
+        // Console.WriteLine(a); 
+
+        //    int a=10;
+        //  int b=a;
+        //   a/=5;
+        // Console.WriteLine(a); 
+
+        //For loop
+//
+    //    int add=0;
+    //    int i;
+    //    for(i=1;i<=10;i++)
+    //    {
+    //        add=add+i;
+         
+    //    }
+    //    Console.WriteLine(add);
+        
+        }
+    }
+
+}
